ef132ce3c246936fd2205da76333ff249f4256e6
Stephan Matthes <stephan.matthes@webinstrument.de>
2018-09-19 21:55:18 +0200

    Counter hinzugefügt

--- src/Loop1.java
+++ src/Loop1.java
@@ -1,15 +1,21 @@
 
 public class Loop1 {
-    
+
+    private static volatile int counter = 0;
+
+    private static int getIncrementedCounter() {
+        return ++counter;
+    }
+
     public static void run(String name) {
-        for(int i=1; i<=100; i++) { 
-            System.out.println(name + " (" + i + ")"); 
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(getIncrementedCounter() + ": " + name + " (" + i + ")");
         }
     }
 
     public static void main(String[] args) {
-        new Thread(() -> run("Thread 1")).start();        
-        new Thread(() -> run("Thread 2")).start();        
+        new Thread(() -> run("Thread 1")).start();
+        new Thread(() -> run("Thread 2")).start();
         new Thread(() -> run("Thread 3")).start();
     }
 
