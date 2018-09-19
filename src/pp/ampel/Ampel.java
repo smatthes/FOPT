3fa2356665caaf727b411008e870b02b8dcf2a0d
Stephan Matthes <stephan.matthes@webinstrument.de>
2018-09-19 22:02:20 +0200

    Initialisierung + Interface Ampel

---
+++ src/pp/ampel/Ampel.java
@@ -0,0 +1,13 @@
+package pp.ampel;
+
+public interface Ampel {
+
+    public void schalteRot();
+
+    public void schalteGruen();
+
+    public void passieren();
+
+    public int wartendeFahrzeuge();
+
+}
