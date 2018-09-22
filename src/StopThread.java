9993061bb746d0e904d3977e5b8b951bd3b5d5d4
Stephan Matthes <stephan.matthes@webinstrument.de>
2018-09-22 11:51:31 +0200

    Code aus dem Buch

---
+++ src/StopThread.java
@@ -0,0 +1,41 @@
+
+public class StopThread extends Thread {
+    
+    private boolean stopped = false;
+    
+    public StopThread() {
+        start();
+    }
+    
+    public synchronized void stopThread() {
+        stopped = true;
+    }
+    
+    public synchronized boolean isStopped() {
+        return stopped;
+    }
+    
+    public void run() {
+        int i = 0;
+        
+        while(!isStopped()) {
+            i++;
+            System.out.println("Hallo Welt (" + i + ")");
+        }
+        System.out.println("Thread endet jetzt ...");
+    }
+
+    public static void main(String[] args) {
+        StopThread st = new StopThread();
+        
+        try {
+            Thread.sleep(5000);
+        }
+        catch(InterruptedException ex) {
+            System.err.println(ex.getLocalizedMessage());
+        }
+        
+        st.stopThread();
+    }
+
+}
