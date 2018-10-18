b6cd835c004853ce1c788f4174c9b0ea6205cdb7
Stephan Matthes <stephan.matthes@webinstrument.de>
2018-10-18 08:39:52 +0200

    Initialisierung

---
+++ src/pp/synchstacksem/Semaphore.java
@@ -0,0 +1,30 @@
+package pp.synchstacksem;
+
+public class Semaphore {
+
+    private int value;
+    
+    public Semaphore(int init) {
+        if(init < 0) {
+            throw new IllegalArgumentException("Parameter init < 0");
+        }
+        
+        value = init;
+    }
+    
+    public synchronized void p() {
+        while(value == 0) {
+            try {
+                wait();
+            } catch(InterruptedException ex) {}
+        }
+        
+        value--;
+    }
+    
+    public synchronized void v() {
+        value++;
+        notify();
+    }
+    
+}
